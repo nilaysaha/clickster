58745084e2843267d9b8927e804604b4272cad7a
Nilay Saha <saha.nilay@gmail.com>
2023-07-21 13:37:15 +0200

    changes

---
+++ src/getDbClient.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv')
+const { Client } = require('pg')
+
+dotenv.config()
+
+const getDbClient = () => {
+    
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || ''),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  })
+
+  return client
+}
+
+
+module.exports = { getDbClient }
